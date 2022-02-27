5d1e1720a6c869f4d3d02fc055799d273443d0f7
leandro angeli <kurco91@hotmail.com>
2022-02-27 16:31:14 -0300

    footer and about section made

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,42 @@
 import React from 'react';
-
+import Container from '@mui/material/Container';
+import { Typography, Box, Divider, List, ListItem } from '@mui/material';
 const About = () => {
-	return <div>About</div>;
+	return (
+		<Container maxWidth="lg" sx={{ bgcolor: 'white' }}>
+			<Box px={10} py={15}>
+				<Typography paragraph>
+					Diseñado y programado por Leandro Angeli , 2022 , Buenos Aires,
+					Argentina
+					<List>
+						Recursos Utilizados:
+						<ListItem>React</ListItem>
+						<ListItem>Material UI</ListItem>
+						<ListItem>Data fetched desde pokeapi.co</ListItem>
+						<ListItem>Fuse</ListItem>
+						<ListItem>TinyColor</ListItem>
+						<ListItem>Axios</ListItem>
+						<ListItem>React router dom</ListItem>
+					</List>
+				</Typography>
+				<Divider></Divider>
+				<Typography paragraph mt={3}>
+					Designed y programmed by Leandro Angeli , 2022 , Buenos Aires,
+					Argentina
+					<List>
+						Resources I used:
+						<ListItem>React</ListItem>
+						<ListItem>Material UI</ListItem>
+						<ListItem>Data fetched from pokeapi.co</ListItem>
+						<ListItem>Fuse</ListItem>
+						<ListItem>TinyColor</ListItem>
+						<ListItem>Axios</ListItem>
+						<ListItem>React router dom</ListItem>
+					</List>
+				</Typography>
+			</Box>
+		</Container>
+	);
 };
 
 export default About;
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import CallIcon from '@mui/icons-material/Call';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { useTheme } from '@mui/styles';
 
 const Footer = () => {
@@ -28,7 +33,24 @@ const Footer = () => {
 				}}
 			>
 				By Leandro Angeli © 2022 ,Buenos Aires Argentina{' '}
-				<LinkedInIcon sx={{ ml: 2 }}></LinkedInIcon>
+				<a href="https://www.linkedin.com/in/leandro-angeli-787b7822b/">
+					<LinkedInIcon sx={{ ml: 2 }}></LinkedInIcon>
+				</a>
+				<a href="https://github.com/Leandro-Andres-Angeli">
+					<GitHubIcon sx={{ ml: 2 }}></GitHubIcon>
+				</a>
+				<a href="mailto:leandroandresangeli@gmail.com">
+					<MailIcon sx={{ ml: 2 }}></MailIcon>
+				</a>
+				<a href="https://telegram.me/L34nndr0">
+					<TelegramIcon sx={{ ml: 2 }}></TelegramIcon>
+				</a>
+				<a href="https://wa.me/541159066928">
+					<WhatsAppIcon sx={{ ml: 2 }}></WhatsAppIcon>
+				</a>
+				<a href="tel:+541159066928">
+					<CallIcon sx={{ ml: 2 }}></CallIcon>
+				</a>
 			</Typography>
 		</Container>
 	);
